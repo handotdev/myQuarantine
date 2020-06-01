5f8d0753145b67d82e9a542017d5b32cdc308774
Han Wang <hyw2@cornell.edu>
2020-05-31 20:50:51 -0700

    Episode 6 start

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Card from './components/Card';
 import Profile from './screens/Profile';
 import Discover from './screens/Discover';
 
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    // <Profile />
-    <Discover />
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Discover') {
+              iconName = 'ios-compass';
+            } else if (route.name === 'Profile') {
+              iconName = 'ios-person';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#FF6767',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Discover" component={Discover} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
